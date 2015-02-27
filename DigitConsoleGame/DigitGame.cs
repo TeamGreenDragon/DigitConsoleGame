c9cc35ea53dd7e0c36feb4ab32db1d8b34363cd7
anabogdanova <ana.bogdanova73@gmail.com>
2015-02-28 00:16:07 +0200

    Comments aded to the code

--- DigitConsoleGame/DigitGame.cs
+++ DigitConsoleGame/DigitGame.cs
@@ -10,12 +10,12 @@ class DigitGame
 { 
     static public int i = 2, j = 2;
     static public int swapVar;
-    static int moves = 0;
-    static string Name;
-    static public int[,] array = new int[3, 3];
+    static int moves = 0;                       // Counts the moves 
+    static string Name;                         // Stores player's name
+    static public int[,] array = new int[3, 3]; // Game board values
     static public string path = @"..\..\Images\scores.txt";
 
-    public static void PrintScores()
+    public static void PrintScores()            // Reads the scores from the text file and displays them
     {
         Console.Clear();
         string score = System.IO.File.ReadAllText(@"..\..\Images\ScoreboardImage.txt");
@@ -23,10 +23,10 @@ class DigitGame
         Console.Write("Name".PadRight(15));
         Console.WriteLine("Number of Attempts");
         int position = 1;
-        List<string> scoreList = File.ReadAllLines(path).ToList();
-        foreach (string separateScore in scoreList)
+        List<string> scoreList = File.ReadAllLines(path).ToList();  // Reads the scores from the text file
+        foreach (string separateScore in scoreList)                
         {
-            if (position <= 10)
+            if (position <= 10)                                      //... and displays the first 10 of them
             {
                 Console.WriteLine(position.ToString().PadLeft(2, ' ') + ". " + separateScore);
             }
@@ -35,39 +35,39 @@ class DigitGame
         }
     }
 
-    public static void ScoreRecords(int score, string name)
+    public static void ScoreRecords(int score, string name)         // Writes the results in a text file
     {
         List<string> scoreList;
-        if (File.Exists(path))
+        if (File.Exists(path))                                      // If the file exists
         {
-            scoreList = File.ReadAllLines(path).ToList();
+            scoreList = File.ReadAllLines(path).ToList();           // reads all scores in a list -> scoreList
         }
         else
         {
-            scoreList = new List<string>();
+            scoreList = new List<string>();                         // else creates a new list for the results -> scoreList
         }
-        scoreList.Add(name.PadRight(10, ' ') + "\t" + score.ToString());
-        var sortedScoreList = scoreList.OrderBy(ss => int.Parse(ss.Substring(ss.LastIndexOf("\t") + 1)));
-        File.WriteAllLines(path, sortedScoreList.ToArray());
+        scoreList.Add(name.PadRight(10, ' ') + "\t" + score.ToString()); // ... writes the new result in it
+        var sortedScoreList = scoreList.OrderBy(ss => int.Parse(ss.Substring(ss.LastIndexOf("\t") + 1)));  // ... sorts it
+        File.WriteAllLines(path, sortedScoreList.ToArray());            //and writes it back to the text file
     }
 
-    static public void Keys()
+    static public void Keys()                                       //Reads and performs the player's moves (arrows)
     {
-        try
+        try                                                         //Uses Exception
         {
-            for (i = 0; i < 3; i++)
+            for (i = 0; i < 3; i++)                                 //
             {
                 for (j = 0; j < 3; j++)
                 {
                     while (array[i, j] == 0)
                     {
-                        ConsoleKeyInfo move;
-                        Console.Beep();
-                        move = Console.ReadKey();
-                        Ends(move);
-                        switch (move.Key)
+                        ConsoleKeyInfo move;                        // the variable move stores the current key pressed
+                        Console.Beep();                             //sound
+                        move = Console.ReadKey();                   // Reads the player's input
+                        Ends(move);                                 // Checks if the player had choosen to end the game
+                        switch (move.Key)                           // if the game continues, reads the next move
                         {
-                            case ConsoleKey.LeftArrow:
+                            case ConsoleKey.LeftArrow:              // <-
                                
                                 swapVar = array[i, j];
                                 array[i, j] = array[i, j - 1];
@@ -75,7 +75,7 @@ class DigitGame
                                 j = j - 1;
                                 moves++;
                                 break;
-                            case ConsoleKey.UpArrow:
+                            case ConsoleKey.UpArrow:                // ^
                                 
                                 swapVar = array[i, j];
                                 array[i, j] = array[i - 1, j];
@@ -83,7 +83,7 @@ class DigitGame
                                 i = i - 1;
                                 moves++;
                                 break;
-                            case ConsoleKey.DownArrow:
+                            case ConsoleKey.DownArrow:              // v
                                 
                                 swapVar = array[i, j];
                                 array[i, j] = array[i + 1, j];
@@ -91,7 +91,7 @@ class DigitGame
                                 i = i + 1;
                                 moves++;
                                 break;
-                            case ConsoleKey.RightArrow:
+                            case ConsoleKey.RightArrow:             // ->
                                 
                                 swapVar = array[i, j];
                                 array[i, j] = array[i, j + 1];
@@ -100,7 +100,7 @@ class DigitGame
                                 moves++;
                                 break;
                             default:
-                                throw new ArgumentException("Invalid key");
+                                throw new ArgumentException("Invalid key"); // invalid key
                         }
                         Values();
                         Check();
@@ -122,7 +122,7 @@ class DigitGame
         
     static Random random = new Random();
 
-    static void Shuffle<T>(T[] array)
+    static void Shuffle<T>(T[] array)                       //Shuffles the numbers for the hard level
     {
         int n = array.Length;
         for (int i = 0; i < n; i++)
@@ -134,7 +134,7 @@ class DigitGame
         }
     }
 
-    static public void InitializeEasy() // Sets initial values in the array
+    static public void InitializeEasy() // Sets initial values in the array for the easy level
     {
         array[0, 0] = 1;
         array[0, 1] = 4;
@@ -147,7 +147,7 @@ class DigitGame
         array[2, 2] = 0;
     }
 
-    static public void InitializeHard() // Sets initial values in the array
+    static public void InitializeHard() // Sets initial values in the array for the hard level
     {
         int[] values =
         {
@@ -171,7 +171,7 @@ class DigitGame
         }
     }
   
-    static public void Check()
+    static public void Check()              //Check if the player wins the game, displays Congrats screen, plays sound
     {
         if (array[0, 0] == 0 && array[0, 1] == 1 && array[0, 2] == 2 && array[1, 0] == 3 && array[1, 1] == 4 && array[1, 2] == 5 && array[2, 0] == 6 && array[2, 1] == 7 && array[2, 2] == 8)
         {
@@ -189,7 +189,7 @@ class DigitGame
         }
     }
 
-    static void Ends(ConsoleKeyInfo move)
+    static void Ends(ConsoleKeyInfo move)               //Checks if the player's choice is to exit the game
     {
         if (move.Key == ConsoleKey.Q)
         {
@@ -198,6 +198,7 @@ class DigitGame
             if (move.Key == ConsoleKey.Y)
             {
                 PrintScores();
+               // Console.ReadKey();
                 Environment.Exit(0);
             }
             if (move.Key == ConsoleKey.N)
@@ -216,13 +217,13 @@ class DigitGame
         }
     }
 
-    static public void Show()
+    static public void Show()           // Shows the text "Press Q for exit, N for new game"
     {
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("\nPress Q for exit, N for new game");
     }
 
-    static public void Values()
+    static public void Values()            //Draws the game board
     {
         Console.Clear();
         Console.ForegroundColor = ConsoleColor.Green;
@@ -234,18 +235,18 @@ class DigitGame
         Console.WriteLine("\n\n\t\t" + new string('-', 50) + "\n\n");
         Console.ForegroundColor = ConsoleColor.Red;
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++)                                      //Draws the game board
         {
             for (int j = 0; j < 3; j++)
             {
                 if (array[i, j] == 0)
                 {
-                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Cyan;        // The 0 symbol in Cyan color
                     Console.Write("{0,20}", array[i, j]);
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.Magenta;      // The other symbols in Magenta color
                     Console.Write("{0,20}", array[i, j]);
                 }
             }
@@ -254,12 +255,12 @@ class DigitGame
         Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("\n\n\t\t" + new string('-', 50));
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("\n\n\t\t\t\t\t\t\tAll The Best ");
+        Console.WriteLine("\n\n\t\t\t\t\t\t\tAll The Best ");           // All the best, Player!
         Console.ForegroundColor = ConsoleColor.Magenta;
         Console.WriteLine("\n\t\t\t\t\t\t\t  " + Name);
         Show();
     }
-    static public void Choise()
+    static public void Choise()                                     //Reads the player's choice for level - Easy, Hard
     {
         ConsoleKeyInfo modeChoice;
         modeChoice = Console.ReadKey();
@@ -272,13 +273,13 @@ class DigitGame
             InitializeHard();
         }
     }
-    static void Main()
+    static void Main()                                          // Main method
     {
         //game name title
         Console.ForegroundColor = ConsoleColor.Green;
         string title = System.IO.File.ReadAllText(@"..\..\Images\Title.txt");
-        Console.WriteLine(title);
-        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine(title);                           //Welcome screen
+        Console.ForegroundColor = ConsoleColor.DarkGray;    // Rules
         Console.WriteLine("Game Rules:\nArrange the numbers from 0-8 in right order by pressing the arrow keys.");
         Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine();
@@ -286,9 +287,9 @@ class DigitGame
         Console.ForegroundColor = ConsoleColor.Red;
         Name = Console.ReadLine();
         Console.WriteLine("Press E for easy game and H for hard mode.");
-        Choise();
-        Values();
-        Keys();
+        Choise();                                            //Reads the player's choice for level - Easy, Hard
+        Values();                                           //Draws the game board
+        Keys();                                              // Reads player's input
         Console.ReadLine();
     }
 }
